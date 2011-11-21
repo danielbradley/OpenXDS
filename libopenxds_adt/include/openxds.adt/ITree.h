f80d9f293bcfab35669e2b969b2021574944cd7f
Daniel Bradley <daniel.bradley@islabs.com>
2011-11-21 14:02:53 +1000

    Extracted ITree to mtx.

--- libopenxds_adt/include/openxds.adt/ITree.h
+++ libopenxds_adt/include/openxds.adt/ITree.h
@@ -15,23 +15,24 @@ public:
 	virtual       ~ITree() {}
 
 	virtual       IPosition<E>*      addRoot( E* value ) = 0;
-	virtual       IPosition<E>*     addChild( IPosition<E>& p, E* value ) = 0;
-	virtual       IPosition<E>*   addSubtree( IPosition<E>& p, ITree* value ) = 0;
-	virtual       E*                 replace( IPosition<E>& p, E* value ) = 0;
-	virtual       E*                  remove( IPosition<E>* p ) = 0;
-	virtual       ITree<E>*     removeAsTree( IPosition<E>* p ) = 0;
+	virtual       IPosition<E>*     addChild( IPosition<E>& p, E*        value ) = 0;
+	virtual       IPosition<E>*   addSubtree( IPosition<E>& p, ITree<E>* value ) = 0;
+	virtual       E*                 replace( IPosition<E>& p, E*        value ) = 0;
+	virtual       E*                  remove( IPosition<E>* p )                  = 0;
+	virtual       ITree<E>*     removeAsTree( IPosition<E>* p )                  = 0;
 
-	virtual       IPosition<E>*         root() const throw (openxds::exceptions::NoSuchElementException*) = 0;
+	virtual       IPosition<E>*         root()                  const throw (openxds::exceptions::NoSuchElementException*) = 0;
 	virtual       IPosition<E>*       parent( IPosition<E>& p ) const throw (openxds::exceptions::NoSuchElementException*) = 0;
 	virtual       IPIterator<E>*    children( IPosition<E>& p ) const = 0;
 	virtual       bool                isRoot( IPosition<E>& p ) const = 0;
 	virtual       bool            isInternal( IPosition<E>& p ) const = 0;
 	virtual       bool            isExternal( IPosition<E>& p ) const = 0;
 	virtual       bool             hasParent( IPosition<E>& p ) const = 0;
-	virtual       bool               isEmpty() const = 0;
-	virtual       int                   size() const = 0;
+	virtual       bool               isEmpty()                  const = 0;
+	virtual       int                   size()                  const = 0;
 }; 
 
 };};
 
 #endif
+
