3985b7403b0b8e8caffcb283fdeeb52aeb162c9c
Daniel Bradley <daniel.bradley@islabs.com>
2012-04-19 19:13:23 +1000

    Added IMap to types file.

--- libopenxds_adt/include/openxds.adt.h
+++ libopenxds_adt/include/openxds.adt.h
@@ -13,6 +13,7 @@ template<class E> class PUBLIC IEntry;
 template<class E> class PUBLIC IGeneralTour;
 template<class E> class PUBLIC IIterator;
 template<class E> class PUBLIC IList;
+template<class E> class PUBLIC IMap;
 template<class E> class PUBLIC IPIterator;
 template<class E> class PUBLIC IPosition;
 template<class E> class PUBLIC IStack;
